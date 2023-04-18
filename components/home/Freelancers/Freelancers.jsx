3c9d4dd462ad79a52f5a24a439b9998d7fd4a4cf
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-04-19 01:17:02 +0200

    Finished the Components setup

---
+++ components/home/Freelancers/Freelancers.jsx
@@ -0,0 +1,7 @@
+const Freelancers = () => {
+    return (
+        <></>
+    )
+}
+
+export default Freelancers;
---
+++ components/home/Tribute/Tribute.jsx
@@ -0,0 +1,7 @@
+const Tribute = () => {
+    return (
+        <></>
+    )
+}
+
+export default Tribute;
