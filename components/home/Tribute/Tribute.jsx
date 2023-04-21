661889d9e72bc90dbe670a21baeaa65429d72930
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-04-21 11:36:30 +0200

    Completed the Tribute

--- components/home/Tribute/Tribute.jsx
+++ components/home/Tribute/Tribute.jsx
@@ -1,6 +1,15 @@
+import { Box, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
 const Tribute = () => {
+    const theme = useTheme();
+
     return (
-        <></>
+        <Box sx={{width: "100%", height: "300px", backgroundImage: "url('/images/coffee.png')", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundAttachment: "fixed", display: "grid", placeItems: "center"}}>
+            <Typography variant="h1" color={theme.palette.accent.light}>
+                منصة عربية واعدة لتوصيل الشركات بأصحاب الأعمال
+            </Typography>
+        </Box>
     )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/home/Hero/Hero";
 import Features from "@/components/home/Features/Features";
 import Freelancers from "@/components/home/Freelancers/Freelancers";
+import Tribute from "@/components/home/Tribute/Tribute";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <Hero />
         <Features />
         <Freelancers />
+        <Tribute />
       </>
   )
 }
