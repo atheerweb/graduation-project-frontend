4d2ee2896c3ba7bb4fd58f190a23794ba27001a5
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-04-26 05:11:11 +0200

    Completed the Hero of the courses page

---
+++ components/courses/Hero.jsx
@@ -0,0 +1,24 @@
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import styles from "@/styles/modules/courses/hero.module.css";
+import { useTheme } from "@mui/material/styles";
+
+const Hero = () => {
+    const theme = useTheme();
+
+    return (
+        <Box className={styles.courses}>
+            <Image src={"/images/courses_hero.png"} width={338} height={425} />
+            <Box className={styles.coursesTypographyContainer}>
+                <Typography variant="h1">
+                    طريقك للنجاح يبدأ من هنا
+                </Typography>
+                <Typography variant="h3" color={theme.palette.accent.primary}>
+                    أفضل المعلمين من جميع أنحاء مصر و استمتع بأفضل العروص و الأسعار
+                </Typography>
+            </Box>
+        </Box>
+    )
+}
+
+export default Hero;
---
+++ pages/courses/index.jsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/courses/Hero";
+
+const Courses = () => {
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
+
+export default Courses;
