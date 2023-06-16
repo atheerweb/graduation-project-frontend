69bd3aad2d1687e93fda97aab41f4223fd2e6996
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-06-16 22:35:17 +0300

    Completed the API for the Freelancers in Homepage

--- components/home/Freelancers/Freelancers.jsx
+++ components/home/Freelancers/Freelancers.jsx
@@ -5,20 +5,20 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 // Hooks
-import { useSelector } from "react-redux";
+import { useApi } from "@/lib/hooks";
 // CSS Modules
 import styles from "@/styles/modules/home/freelancers.module.css";
 
 const Freelancers = () => {
-    const freelancersCards = useSelector(state => state.constants.value.freelancersCards);
-    
+    const freelancers = useApi("/freelance/top-5-freelancers/");
+
     return (
         <Box className={styles.freelancers}>
             <Typography variant="h2" textAlign="center">أكثر المستقلين طلباً</Typography>
             <Grid className={styles.freelancersCardsGrid}>
                 {
-                    freelancersCards.map((freelancer, index) => (
-                        <Cards key={index} title={freelancer.title} body={freelancer.body} />
+                    freelancers.map((freelancer, index) => (
+                        <Cards key={index} title={`${freelancer.first_name} ${freelancer.last_name}`} body={freelancer.email} />
                     ))
                 }
             </Grid>
