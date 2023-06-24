a81da8001707be2f6513e61c1a5af2418fcdf55e
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2023-06-24 19:28:01 +0300

    filters fix

--- components/freelancers/Main/Main.jsx
+++ components/freelancers/Main/Main.jsx
@@ -7,27 +7,28 @@ import { useSelector } from "react-redux";
 // CSS Modules
 import styles from "@/styles/modules/freelancers/freelancers.module.css";
 
-
 const Main = (props) => {
-    const filteredCards = useSelector(state => state.api.value.allFreelancers).filter(card => {
-        const fullName = `${card.first_name} ${card.last_name}`;
-
-        if (fullName.toLowerCase().includes(props.filters.title.toLowerCase())) {
-            if (card.ratings >= props.filters.ratings) {
-                return card;
-            }
+  const filteredCards = useSelector(
+    (state) => state.api.value.allFreelancers
+  ).filter((card) => {
+    if (props.filters.title) {
+      const fullName = `${card.first_name} ${card.last_name}`;
+      if (fullName.toLowerCase().includes(props.filters.title.toLowerCase())) {
+        if (card.ratings >= props.filters.ratings) {
+          return card;
         }
-    });
+      }
+    }
+    return card;
+  });
 
-    return (
-        <Grid className={styles.mainGrid}>
-            {
-                filteredCards.map((card, index) => (
-                    <Cards key={index} card={card} />
-                ))
-            }
-        </Grid>
-    )
-}
+  return (
+    <Grid className={styles.mainGrid}>
+      {filteredCards.map((card, index) => (
+        <Cards key={index} card={card} />
+      ))}
+    </Grid>
+  );
+};
 
 export default Main;
