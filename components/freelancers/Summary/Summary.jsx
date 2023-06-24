4f15e397154d48cbb2b90c95e90fab4b0806b4a0
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-06-25 02:55:10 +0300

    Freelancer Avatar

--- components/freelancers/Summary/Summary.jsx
+++ components/freelancers/Summary/Summary.jsx
@@ -1,21 +1,25 @@
 // MUI Components
+import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-// Next Components
-import Image from "next/image";
 // Hooks
 import { useSelector } from "react-redux";
 // CSS Modules
 import styles from "@/styles/modules/freelancers/summary.module.css";
+import { useApi } from "@/lib/hooks";
+import { useRouter } from "next/router";
 
 const Summary = () => {
+    const { query: { username } } = useRouter();
+    const freelancers = useApi("/freelance/all-freelancers/");
+    const freelancerImage = freelancers && freelancers.filter(freelancer => freelancer.username === username)[0];
     const freelancer = useSelector(state => state.api.value.oneFreelancer);
     const summaryStacks = useSelector(state => state.constants.value.summaryStacks);
     
     return (
         <Box className={styles.summary}>
-            <Image height={123} width={123} src="/images/freelancers_summary_icon.png" alt="Freelancer" />
+            <Avatar sx={{height: 123, width: 123}} src={freelancerImage && freelancerImage.image_url || "/images/freelancers_summary_icon.png"} alt="Freelancer" />
             <Typography variant="h2">
                 {freelancer.name}
             </Typography>
